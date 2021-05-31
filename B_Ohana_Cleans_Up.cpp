c7371d84cc5cf62f94ae62ab4ad95329295807ee
ramumaha <62795822+ramumaha@users.noreply.github.com>
2021-05-31 15:42:48 +0530

    Add files via upload

---
+++ B_Ohana_Cleans_Up.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+#define pii pair<int, int>
+#define mp make_pair
+#define f first
+#define s second
+#define mii map<int, int>
+#define msi map<string,int>
+#define mss map<string,string>
+#define mci map<char,int>
+#define mic map<int,char>
+#define loop(i,a,b) for(int i=a;i<b;i++)
+#define clr(x) memset(x, 0, sizeof(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define sort_v(a) sort(a.begin(),a.end())
+#define sort_a(a,n) sort(a,a+n)
+#define reverse(a) reverse(a.begin(),a.end())
+#define pie 3.1415926536;
+
+void solve()
+{ll int n,c=0;
+cin>>n;
+string str;
+map<string,int>mp;
+for(ll int i=0;i<n;i++){
+    cin>>str;
+    mp[str]++;
+}
+for(auto i:mp){
+    if(i.second>c){
+        c=i.second;
+    }
+}
+cout<<c;
+
+}
+
+
+
+int main()
+{
+        ios:: sync_with_stdio(false); 
+    cin.tie(nullptr); cout.tie(nullptr);
+    cout<<fixed; cout<<setprecision(10);
+ 
+    int test_cases = 1;
+    //cin>>test_cases;
+    while(test_cases--)
+    {
+        solve();
+    }
+}

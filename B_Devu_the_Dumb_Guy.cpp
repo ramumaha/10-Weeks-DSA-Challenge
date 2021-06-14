5f03c4cc0a871268b7844742d58d4eaada81cd27
ramumaha <62795822+ramumaha@users.noreply.github.com>
2021-06-14 18:56:45 +0530

    Add files via upload

---
+++ B_Devu_the_Dumb_Guy.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+#define pii pair<int, int>
+#define mp make_pair
+#define f first
+#define s second
+#define mii map<int, int>
+#define msi map<string,int>
+#define mss map<string,string>
+#define mci map<char,int>
+#define mic map<int,char>
+#define loop(i,a,b) for(int i=a;i<b;i++)
+#define clr(x) memset(x, 0, sizeof(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define sort_v(a) sort(a.begin(),a.end())
+#define sort_a(a,n) sort(a,a+n)
+#define reverse(a) reverse(a.begin(),a.end())
+#define pie 3.1415926536;
+
+void solve()
+{ll int n,x,c=0;
+cin>>n>>x;
+vector<ll int>arr(n);
+loop(i,0,n){cin>>arr[i];}
+sort_v(arr);
+if(x==1){
+    loop(i,0,n){
+        c+=arr[i];
+    }
+    cout<<c<<"\n";
+    return;
+}
+loop(i,0,n){
+    c+=arr[i]*x;
+    if(x>1){
+        x--;
+    }
+    else{
+        x=1;
+    }
+}
+cout<<c<<"\n";
+}
+
+
+
+int main()
+{
+        ios:: sync_with_stdio(false); 
+    cin.tie(nullptr); cout.tie(nullptr);
+    cout<<fixed; cout<<setprecision(10);
+ 
+    int test_cases = 1;
+    //cin>>test_cases;
+    while(test_cases--)
+    {
+        solve();
+    }
+}
